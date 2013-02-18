495c8de450b3ec74e38e17f9ca8842cd5d0f55d2
Paul Wood III <paul.wood.iii@gmail.com>
2013-02-18 15:28:33 +0800

    application files

---
+++ app.js
@@ -0,0 +1,44 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/inbi', function(req, res){
+    res.redirect('walkin://inbi/amount:60/');
+});
+app.get('/error', function(req, res){
+    res.redirect('walkin://error/message:ErrorMessageHere/errorTitle:Title/');
+});
+app.get('/finish', function(req, res){
+    res.redirect('walkin://');
+});
+app.get('/badge', function(req, res){
+    res.redirect('walkin://badge/imageurl:"http://badgeiamgeurl"/badgeText:text');
+});
+app.get('/deal', function(req, res){
+    res.redirect('walkin://deal/dealId:101');
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
