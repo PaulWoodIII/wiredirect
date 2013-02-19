da6626e1d6f649d5aab1debe8f5b76e9c6f32b04
Paul Wood III <paul.wood.iii@gmail.com>
2013-02-19 10:19:35 +0800

    Added validation to the index page

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,18 @@
 exports.index = function(req, res){
-    res.render('index', { title: 'Wire Router' });
+    if(!req.query.token){
+        res.send(404,'Error validating token');
+    }
+    else if(!req.query.device){
+        res.send(404,'Error validating device');
+    }
+    else if(!req.query.version){
+        res.send(404,'Error validating version');
+    }
+    else if (req.query.token && req.query.device && req.query.version){
+        res.render('index', { title: 'Wire Router' });
+    }
+    else{
+        // Catch all doubt it should get here though
+        res.send(404,'Error validating');
+    }
 };
